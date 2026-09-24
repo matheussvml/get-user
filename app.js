89bbf37af4137fd9806a043c8f0bf1c49d99bdf4
Matheus Vasconcelos <vmlmatije@gmail>
2026-09-23 22:15:00 -0300

    feat:implementar pesquisa por rota

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+const input = document.querySelector('input');
 const button = document.querySelector('button');
+const list = document.querySelector('ul');
 
 
 async function handle(){
-    const response = await fetch('http://localhost:3000');
-    const data = await response.json();
+
+
+    try {
+        const response = await fetch('http://localhost:3000/' + input.value);
+        const datas = await response.json();
+
+        if (!response.ok){
+            throw new Error(response.status + ' Rota não existente.');
+        }
     
-    console.log(data);
+        list.textContent = '';
+        
+        for (let i = 0; i < datas.length; i++) {
+            const item = document.createElement('li');
+            item.textContent = (datas[i].name || datas[i].product) + ' | ' + (datas[i].email || datas[i].preco)
+            list.appendChild(item);
+        }
+        
+    } catch (error) {
+        list.textContent = 'Erro' + error.message;
+    }
+
 }
 
 button.addEventListener('click', handle);
 
-// handle();
--- server.js
+++ server.js
@@ -3,11 +3,49 @@ const http = require('node:http');
 
 http.createServer((request, response)=> {
     response.setHeader('Access-Control-Allow-Origin', '*');
-    response.writeHead(200,{'content-type':'application/json'});
-    response.end(JSON.stringify({
-    name: 'Alex Bessa',
-    email: 'alex@email.com'
-}));
+    
+    if(request.url === '/users'){
+        response.writeHead(
+            200,
+            {'content-type': 'application/json'}
+        );
+            response.end(JSON.stringify([{
+                name: 'Alex Bessa',
+                email: 'alex@email.com'
+            } , {
+                name: 'Valdiana Bessa',
+                email: 'valdiana@email.com'
+
+            } , {
+                name: 'Ana Bessa',
+                email: 'ana@email.com'
+            }]));
+            return;
+        }
+    if(request.url === '/products'){
+        response.writeHead(
+            200,
+            {'content-type': 'application/json'}
+        );
+        response.end(JSON.stringify([
+            {
+                product: 'Celular', 
+                preco: 5000 
+            },
+            {
+                product: 'TV', 
+                preco: 6199
+            },
+            {
+                product: 'Relogio', 
+                preco: 300
+            }
+        ]));
+        return;
+    }
+    response.writeHead(404,{'content-type':'application/json'});
+
+    response.end(JSON.stringify({message: ' Não existente.'}));
 }).listen(3000);
 
 
